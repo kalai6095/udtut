6eeee7220d1e0bb787d90285bebcf214e1793503
kalaiselvan <kalai6095@gmail.com>
2020-01-03 07:47:04 +0530

    forms added

--- src/app/example/formexamples/formexamples.component.ts
+++ src/app/example/formexamples/formexamples.component.ts
@@ -11,16 +11,55 @@ export class FormexamplesComponent implements OnInit {
   constructor() {
   }
 
+
   @ViewChild('formElement', {static: false}) formElement: NgForm;
+  answer;
+  genders = ['male', 'female'];
+  submitted = false;
+  user = {
+    username: '',
+    mail: '',
+    question: '',
+    answer: '',
+    gender: ''
+  };
 
   ngOnInit() {
   }
 
   onSubmit() {
     if (this.formElement.valid) {
-      console.log(this.formElement);
+      this.submitted = true;
+      this.user.username = this.formElement.value.userData.username;
+      this.user.mail = this.formElement.value.userData.mail;
+      this.user.answer = this.formElement.value.userData.questionAnswer;
+      this.user.question = this.formElement.value.userData.secret;
+      this.user.gender = this.formElement.value.userData.gender;
+      this.formElement.reset();
     }
 
+  }
+
+  onsuggUsername() {
+    /* this.formElement.setValue({
+       userData: {
+         username: 'kalaiselvan',
+         mail: '',
+         secret: 'pet',
+         questionAnswer: '',
+         gender: 'male'
+       }
+     });*/
+    this.formElement.form.patchValue(
+      {
+        userData: {
+          username: 'kalaiselvan',
+          secret: 'pet',
+          questionAnswer: '',
+          gender: 'male'
+        }
+      }
+    );
   }
 
 }
