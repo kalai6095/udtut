784a2e88b6628773187a3f06a59645ca7474df0e
kalaiselvan <kalai6095@gmail.com>
2020-01-21 23:43:19 +0530

    http added

--- src/app/example/httpexample/httpexample.component.ts
+++ src/app/example/httpexample/httpexample.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-httpexample',
@@ -10,6 +12,7 @@ import {HttpClient} from '@angular/common/http';
 export class HttpexampleComponent implements OnInit {
 
   httpExampleForm: FormGroup;
+  postData: Post[];
 
   constructor(private  http: HttpClient) {
   }
@@ -19,13 +22,34 @@ export class HttpexampleComponent implements OnInit {
       title: new FormControl(null, Validators.required),
       content: new FormControl(null, Validators.required)
     });
+    this.onfetchData();
   }
 
   onSubmit() {
-    alert();
-    this.http.post('https://ang-udtut-8.firebaseio.com/comments', this.httpExampleForm.value).subscribe((data) => {
-      console.log(data);
-    });
+    alert(this.httpExampleForm.value);
+    this.http.post('https://ang-udtut-8.firebaseio.com/posts.json', this.httpExampleForm.value)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  onfetchData() {
+    this.http.get<{ [key: string]: Post }>('https://ang-udtut-8.firebaseio.com/posts.json')
+      .pipe(map((responseData: { [key: string]: Post }) => {
+          const postArray: Post[] = [];
+          // tslint:disable-next-line:forin
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({...responseData[key], id: key});
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe((data) => {
+        console.log(data);
+        this.postData = data;
+      });
   }
 
 }
